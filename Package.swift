b0b936b541c361a2bcff4172d0c971c158518505
Steven Woolgar <steven.x.woolgar@kp.org>
2023-01-24 15:08:46 -0800

    Fixed the issues with why the brew install would not work

--- Package.swift
+++ Package.swift
@@ -1,24 +1,30 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "xcodebuild-to-md",
+    platforms: [
+        .macOS(.v10_15)
+    ],
+
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/davidahouse/XCResultKit",
-        from: "0.5.7")
+        from: "1.0.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
+        .executableTarget(
             name: "xcodebuild-to-md",
             dependencies: ["XCResultKit"]),
         .testTarget(
             name: "xcodebuild-to-mdTests",
             dependencies: ["xcodebuild-to-md"]),
-    ]
+    ],
+
+    swiftLanguageVersions: [.v5]
 )
